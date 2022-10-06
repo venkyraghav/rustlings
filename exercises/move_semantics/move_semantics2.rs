65686ad0a841abaa97b8f807e5f339295128a1a6
Venky Narayanan <venkyraghav@gmail.com>
2022-10-05 21:49:17 -0400

    completed move_semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,14 +2,17 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+    println!("Running option 1");
     option1();
+    println!("Running option 2");
     option2();
+    println!("Running option 3");
     option3();
 }
 
+// TODO Make another, separate version of the data that's in `vec0` and pass that to `fill_vec` instead.
+// DONT KNOW HOW
 fn option1() {
 /*
     let vec0 = Vec::new();
@@ -37,10 +40,13 @@ fn option1_fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec
 }
 
+// Make `fill_vec` borrow its argument instead of taking ownership of it,
+//   and then copy the data within the function in order to return an owned
+//   `Vec<i32>`
 fn option2() {
     let vec0 = Vec::new();
 
-    let mut vec1 = option2_fill_vec(vec0);
+    let mut vec1 = option2_fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -50,14 +56,14 @@ fn option2() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn option2_fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn option2_fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec1 = Vec::new();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    vec1.push(22);
+    vec1.push(44);
+    vec1.push(66);
 
-    vec
+    vec1
 }
 
 // Make `fill_vec` *mutably* borrow a reference to its argument (which will need to be
