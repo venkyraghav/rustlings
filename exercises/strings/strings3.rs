d690ba7c22af29bda985ea358681dad1048f2a62
Venky Narayanan <venkyraghav@gmail.com>
2022-10-07 10:12:17 -0400

    Pre Quiz2

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,19 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    return input.trim().to_string();
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // return format!("{}{}", input, " world!");
+    let mut s = input.to_string();
+    s.push_str(" world!");
+    s
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    return input.replace("cars", "balloons").to_string();
 }
 
 #[cfg(test)]
@@ -37,6 +35,8 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
+        let mut s = "".to_string();
+        
         assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
         assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
     }
